32209189fdab3666a8bd3e1ba08bc3e3078b8c53
Mike Trentman <Mike.Trentman@alveohealth.com>
2016-11-30 14:36:15 -0500

    Modified so as to list any EC2 instances from any available regions for the credentials being used.

--- src/main/java/com/emptylogic/AwsEc2Info.java
+++ src/main/java/com/emptylogic/AwsEc2Info.java
@@ -8,14 +8,13 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.AmazonEC2Exception;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
  
 public class AwsEc2Info
 {
-	private static final String regionName = "us-east-2";
-	
     public static void main(String[] args) throws Exception
     {
 	    AWSCredentials credentials = null;
@@ -26,28 +25,35 @@ public class AwsEc2Info
         } 
         catch (Exception e) 
         {
-            throw new AmazonClientException(
-                    "Could not create credentials!", e);
+            throw new AmazonClientException("Could not create credentials.", e);
         }
         
         AmazonEC2 ec2 = new AmazonEC2Client(credentials);
         int count = 1;
-
-        Region region = Region.getRegion(Regions.fromName(regionName));
-        ec2.setRegion(region);
-
-        DescribeInstancesResult instanceResult = ec2.describeInstances();
-        
-        for (Reservation reservation : instanceResult.getReservations())
+        for (Regions regions : Regions.values())
         {
-            for(Instance instance : reservation.getInstances())
+            Region region = Region.getRegion(Regions.fromName(regions.getName()));
+            ec2.setRegion(region);
+            try 
             {
-                System.out.println("Instance # " + count++
-                    + "\n InstanceId: " + instance.getInstanceId()
-                    + "\n InstanceType: " + instance.getInstanceType()
-                    + "\n Public IP: " + instance.getPublicIpAddress()
-                    + "\n Public DNS: " + instance.getPublicDnsName()
-                    + "\n");
+                DescribeInstancesResult instanceResult = ec2.describeInstances();
+                
+                for (Reservation reservation : instanceResult.getReservations())
+                {
+                    for(Instance instance : reservation.getInstances())
+                    {
+                        System.out.println("Instance # " + count++
+                            + "\n InstanceId: " + instance.getInstanceId()
+                            + "\n InstanceType: " + instance.getInstanceType()
+                            + "\n Public IP: " + instance.getPublicIpAddress()
+                            + "\n Public DNS: " + instance.getPublicDnsName()
+                            + "\n");
+                    }
+                }
+            }
+            catch(AmazonEC2Exception e) 
+            {
+            	//Not authorized so ignore this region
             }
         }
     }
